f664e250662c4bc4bddb70a2b3ca0c00452a3b9e
toare05 <toarete05@gmail.com>
2025-03-04 15:28:23 +0900

    Initial Commit

---
+++ tagidea/tagideaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tagideaApp.swift
+//  tagidea
+//
+//  Created by MINJE JO on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct tagideaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tagideaTests/tagideaTests.swift
@@ -0,0 +1,17 @@
+//
+//  tagideaTests.swift
+//  tagideaTests
+//
+//  Created by MINJE JO on 3/4/25.
+//
+
+import Testing
+@testable import tagidea
+
+struct tagideaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
